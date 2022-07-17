50bfeaecb678089995baba9f60a5ad6afa80a560
tamara.lima <tamara.lima@br.experian.com>
2022-07-17 14:31:44 -0300

    control structure

---
+++ controle/if2.js
@@ -0,0 +1,20 @@
+function teste1(num) {
+    if(num > 7)
+        console.log(num)
+
+    console.log('Final')
+}
+
+teste1(8)
+teste1(6)
+
+function teste2(num){
+    if(num > 7); // n usar ponto e virgula com as estruturas de controle
+    {
+
+        console.log(num)
+    }
+}
+
+teste2(8)
+teste2(6)
