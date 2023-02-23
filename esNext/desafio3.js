1875b011d47f564e2205de47ef6a132683f2e65f
Tamara Lima <tamara.lima@br.experian.com>
2023-02-22 22:34:40 -0300

    #finalizacao do curso

---
+++ esNext/desafio3.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+
+const caminho = path.join(__dirname, 'dados.txt')
+
+function lerArquivo(caminho) {
+    return new Promise(resolve => {
+        fs.readFile(caminho, function (_, conteudo) {
+            resolve(conteudo.toString())
+        })
+    })
+
+}
+
+
+lerArquivo(caminho)
+    .then(info =>
+        console.log(info)
+    )
---
+++ esNext/map.js
@@ -0,0 +1,23 @@
+// a chave pode ser uma funcao, numero, objeto ...
+// diferente do objeto que existe chave e valor, onde a chave é string
+
+const tecnologias = new Map()
+tecnologias.set('react', { framework: false })
+tecnologias.set('angular', { framework: true })
+
+console.log(tecnologias.react)
+console.log(tecnologias.get('react').framework)
+
+const chavesVariadas = new Map([
+    [function () {}, 'Função'],
+    [{}, 'Objeto'],
+    [123, 'Número'],
+])
+
+chavesVariadas.forEach((vl, ch) => {
+    console.log(ch, vl)
+})
+
+console.log(chavesVariadas.has(123))
+chavesVariadas.delete(123)
+console.log(chavesVariadas.has(123))
